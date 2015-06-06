b116dee6913f27a5ae644ef261a031fac2363394
Hideyuki Takei <takehide22@gmail.com>
2015-06-06 20:27:56 +0900

    ignore docker rm

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,17 @@
 require "docker"
 require "serverspec"
 
+if ENV['CIRCLECI']
+  class Docker::Container
+    def remove(options={})
+      # do not delete container
+    end
+    alias_method :delete, :remove
+  end
+end
+
 set :backend, :docker
 set :docker_url, ENV["DOCKER_HOST"]
 set :docker_image, "specinfra_test"
+set :os, :family => 'ubuntu', :arch => 'x86_64'
 
